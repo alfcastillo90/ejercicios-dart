9320ce093a2dca06f3618bc938f569f5c17dd062
Carlos Castillo <cacr1990@gmail.com>
2023-01-11 14:18:53 -0300

    ADD: basic function

---
+++ 03__functions/01_basic_function.dart
@@ -0,0 +1,13 @@
+void main() {
+  voidFunction();
+  var message = regards();
+  print(message);
+}
+
+void voidFunction() {
+  print('this is a void function');
+}
+
+String regards() {
+  return 'Hello Alfredo';
+}
