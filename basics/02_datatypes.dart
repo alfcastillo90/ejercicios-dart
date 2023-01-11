4d55bed6f94a340781b9e44d3b4ddbc9e3aceb01
Carlos Castillo <cacr1990@gmail.com>
2023-01-10 21:45:39 -0300

    ADD: boolean, lists, maps and sets data types

--- basics/02_datatypes.dart
+++ basics/02_datatypes.dart
@@ -17,4 +17,66 @@ main() {
   print(name + ' ' + secondName);
   print(longString);
   print(inlineString);
+
+  // Boolean
+  bool isActive = true;
+  bool isNotActive = !isActive;
+
+  print(isActive);
+  print(isNotActive);
+
+  // List
+
+  List<String> villains = [
+    'Lex',
+    'Red Skull',
+    'Doom',
+    'Orochimaru',
+    'Madara Uchiha'
+  ];
+
+  print(villains);
+
+  villains[0] = 'Lex Luthor';
+
+  // Sets
+
+  var setOfVillains = {
+    'Lex',
+    'Red Skull',
+    'Doom',
+    'Orochimaru',
+    'Madara Uchiha'
+  };
+
+  villains.add('Deidara');
+  villains.add('Deidara');
+  villains.add('Deidara');
+  villains.add('Deidara');
+
+  setOfVillains.add('Deidara');
+  setOfVillains.add('Deidara');
+
+  print('list of villains $villains');
+  print('setOfVillains $setOfVillains');
+
+  var listOfVillainsToSet = villains.toSet();
+  print(listOfVillainsToSet);
+  print(listOfVillainsToSet.toList());
+
+  // Maps
+  Map<String, dynamic> ironMan = {
+    'name': 'Tony Stark',
+    'skills': 'Intelligence and money',
+    'level': 9000
+  };
+
+  print('iron man $ironMan');
+  print(ironMan['level']);
+
+  Map<String, dynamic> captain = new Map();
+  captain.addAll({'name': 'Steve', 'power': 'extreme force', 'level': 9000});
+
+  print('captain america $captain');
+  Map<int, int> tabla = {1: 1, 3: 2};
 }
